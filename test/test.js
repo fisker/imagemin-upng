2e36c43511e2e5982e4be095dc81a1856c8c74ff
fisker <lionkay@gmail.com>
2020-01-26 13:29:53 +0800

    test: update ava to v3

--- test/test.js
+++ test/test.js
@@ -17,7 +17,7 @@ function writeFixture(file, data) {
 }
 
 test('reject on non-buffer', async t => {
-  await t.throwsAsync(upng()(null), TypeError)
+  await t.throwsAsync(upng()(null), {instanceOf: TypeError})
 })
 
 test('optimize PNG', async t => {
