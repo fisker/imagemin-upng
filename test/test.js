658b326bd756adf3e077817bf38c28ade4c60a5b
fisker Cheung <lionkay@gmail.com>
2021-08-05 09:46:23 +0800

    Migrate to ES Module

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
-import fs from 'fs'
-import path from 'path'
+import {promises as fs} from 'node:fs'
+import path from 'node:path'
 import isPng from 'is-png'
 import test from 'ava'
 import imagemin from 'imagemin'
-import upng from '../src'
+import upng from '../index.js'
 
 function getFixture(file) {
-  return path.join(__dirname, 'fixtures', file)
+  return new URL(`fixtures/${file}`, import.meta.url)
 }
 
 function readFixture(file) {
-  return fs.readFileSync(getFixture(file))
+  return fs.readFile(getFixture(file))
 }
 
 function writeFixture(file, data) {
-  return fs.writeFileSync(getFixture(file), data)
+  return fs.writeFile(getFixture(file), data)
 }
 
 test('reject on non-buffer', async (t) => {
@@ -22,7 +22,7 @@ test('reject on non-buffer', async (t) => {
 })
 
 test('optimize PNG', async (t) => {
-  const buffer = readFixture('png.png')
+  const buffer = await readFixture('png.png')
   const data = await upng()(buffer)
 
   writeFixture('png-compressed.png', data)
@@ -32,7 +32,7 @@ test('optimize PNG', async (t) => {
 })
 
 test('optimize APNG', async (t) => {
-  const buffer = readFixture('apng.png')
+  const buffer = await readFixture('apng.png')
   const data = await upng()(buffer)
 
   writeFixture('apng-compressed.png', data)
@@ -42,17 +42,17 @@ test('optimize APNG', async (t) => {
 })
 
 test('support options', async (t) => {
-  const buffer = readFixture('png.png')
+  const buffer = await readFixture('png.png')
   const data = await upng({
     cnum: 128,
   })(buffer)
 
-  t.true(data.length < 28000)
+  t.true(data.length < 28_000)
   t.true(isPng(data))
 })
 
 test('skip optimizing a non-PNG file', async (t) => {
-  const buffer = await fs.readFileSync(__filename)
+  const buffer = await fs.readFile(new URL(import.meta.url))
   const data = await upng()(buffer)
 
   t.is(data.length, buffer.length)
@@ -60,7 +60,7 @@ test('skip optimizing a non-PNG file', async (t) => {
 
 // eslint-disable-next-line ava/no-skip-test
 test.skip('skip optimizing a fully optimized PNG', async (t) => {
-  const buffer = readFixture('png-compressed.png')
+  const buffer = await readFixture('png-compressed.png')
   const data = await upng()(buffer)
   t.is(data.length, buffer.length)
   t.true(isPng(data))
@@ -68,14 +68,14 @@ test.skip('skip optimizing a fully optimized PNG', async (t) => {
 
 // eslint-disable-next-line ava/no-skip-test
 test.skip('skip optimizing a fully optimized APNG', async (t) => {
-  const buffer = readFixture('apng-compressed.png')
+  const buffer = await readFixture('apng-compressed.png')
   const data = await upng()(buffer)
   t.is(data.length, buffer.length)
   t.true(isPng(data))
 })
 
 test('imagemin', async (t) => {
-  const buffer = readFixture('png.png')
+  const buffer = await readFixture('png.png')
   const data = await upng()(buffer)
   const result = await imagemin.buffer(buffer, {
     plugins: [upng()],
